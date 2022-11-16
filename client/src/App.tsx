21a32a34fb72ec7b6527438e64e72ae64a52c8e0
hammyhammer <hammyjacobs@gmail.com>
2022-11-16 12:02:54 -0500

    operators and numbers arranged. need to adjust size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,29 +16,32 @@ function App() {
           <button>xⁱ</button>
         </div>
 
-        <div className='operators'>
-          <button>÷</button>
-          <button>x</button>
-          <button>-</button>
-          <button>+</button>
-          <button>=</button>
+        <div className='bottom'>
+
+
+          <div className='digits'>
+            <button>9</button>
+            <button>8</button>
+            <button>7</button>
+            <button>6</button>
+            <button>5</button>
+            <button>4</button>
+            <button>3</button>
+            <button>2</button>
+            <button>1</button>
+            <button>0</button>
+            <button>.</button>
+          </div>
+
+          <div className='operators'>
+            <button>÷</button>
+            <button>x</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+          </div>
         </div>
 
-        <div className='digits'>
-          <button>9</button>
-          <button>8</button>
-          <button>7</button>
-          <button>6</button>
-          <button>5</button>
-          <button>4</button>
-          <button>3</button>
-          <button>2</button>
-          <button>1</button>
-          <button>0</button>
-          <button>.</button>
-
-
-        </div>
 
 
       </div>
