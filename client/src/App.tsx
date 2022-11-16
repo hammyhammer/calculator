939ddaaced30250b6689d4fc8d960caa4dcc7aa9
hammyhammer <hammyjacobs@gmail.com>
2022-11-16 10:41:04 -0500

    setting up basic UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className='container'>
+        <span className='display'>Numbers Display Here</span>
+        <div className='buttons'>
+          <p className='digits'>1 2 3 4 5 6 7 8 9 0</p>
+          <p className='operators'>+ - / *</p>
+        </div>
+      </div>
     </div>
   );
 }
