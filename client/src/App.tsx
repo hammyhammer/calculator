c8bd62df001ebd5ff04deb66a630fab6e6a60b8e
hammyhammer <hammyjacobs@gmail.com>
2022-11-16 11:47:29 -0500

    added additional buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,33 +9,38 @@ function App() {
           <span>Numbers Display Here</span>
         </div>
 
-        <div className='buttons'>
+        <div className='functions'>
+          <button>AC</button>
+          <button>C</button>
+          <button>{`()`}</button>
+          <button>xⁱ</button>
+        </div>
 
-          <p className='operators'>
-            <button>÷</button>
-            <button>x</button>
-            <button>-</button>
-            <button>+</button>
-            <button>xⁱ</button>
-            <button>=</button>
-          </p>
+        <div className='operators'>
+          <button>÷</button>
+          <button>x</button>
+          <button>-</button>
+          <button>+</button>
+          <button>=</button>
+        </div>
 
-          <p className='digits'>
-            <button>9</button>
-            <button>8</button>
-            <button>7</button>
-            <button>6</button>
-            <button>5</button>
-            <button>4</button>
-            <button>3</button>
-            <button>2</button>
-            <button>1</button>
-            <button>0</button>
-
-          </p>
+        <div className='digits'>
+          <button>9</button>
+          <button>8</button>
+          <button>7</button>
+          <button>6</button>
+          <button>5</button>
+          <button>4</button>
+          <button>3</button>
+          <button>2</button>
+          <button>1</button>
+          <button>0</button>
+          <button>.</button>
 
 
         </div>
+
+
       </div>
     </div>
   );
