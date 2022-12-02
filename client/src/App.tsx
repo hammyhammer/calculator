37c4fa2bfd6718923a94659c354bad2f9feab2f1
hammyhammer <hammyjacobs@gmail.com>
2022-12-02 15:11:57 -0500

    passing props and cleaning old code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,62 +1,63 @@
 import './App.css';
 import React, { ReactNode, useState } from 'react';
 import ButtonFunctions from './components/ButtonFunctions/ButtonFunctions';
+import Buttons from './components/Buttons/Buttons';
+import { clear } from 'console';
 
 
 function App() {
 
-  const operations: string[] = ['.', '-', '+', '*', '/']
+  // const operations: string[] = ['.', '-', '+', '*', '/']
 
-  const [value, setValue] = useState<string>("")
+  // const [value, setValue] = useState<string>("")
 
-  function updateValue(digit: string) {
-    if (operations.includes(digit) && operations.includes(value.slice(-1)) || operations.includes(digit) && value === "") {
-      return;
-    }
-    setValue(value + digit)
-  }
+  // function updateValue(digit: string) {
+  //   if (operations.includes(digit) && operations.includes(value.slice(-1)) || operations.includes(digit) && value === "") {
+  //     return;
+  //   }
+  //   setValue(value + digit)
+  // }
 
-  function clear(): void {
-    setValue("")
-  }
+  // function clear(): void {
+  //   setValue("")
+  // }
 
   // function remove(): void {
   //   setValue(value.slice(0, -1))
   // }
 
-  function calculate(): void {
-    if (operations.includes(value.slice(-1))) {
-      setValue("ERR")
-    }
-    if (value === "ERR") {
-      clear()
-    }
-    else if (!operations.includes(value)) {
-      setValue(eval(value).toString())
-    }
-    console.log(value)
-    // setValue(eval(value).toString())
-  }
+  // function calculate(): void {
+  //   if (operations.includes(value.slice(-1))) {
+  //     setValue("ERR")
+  //   }
+  //   if (value === "ERR") {
+  //     clear()
+  //   }
+  //   else if (!operations.includes(value)) {
+  //     setValue(eval(value).toString())
+  //   }
+  //   console.log(value)
+  //   // setValue(eval(value).toString())
+  // }
 
-  function createDigits(): ReactNode {
-    let digits: JSX.Element[] = [];
-    for (let i = 9; i > 0; i--) {
-      digits.push(<button key={i} onClick={(event: React.MouseEvent<HTMLElement>) => updateValue(`${i}`)}>{i}</button>)
-    }
-    return <div>{digits}</div>
-  }
+  // function createDigits(): ReactNode {
+  //   let digits: JSX.Element[] = [];
+  //   for (let i = 9; i > 0; i--) {
+  //     digits.push(<button key={i} onClick={(event: React.MouseEvent<HTMLElement>) => updateValue(`${i}`)}>{i}</button>)
+  //   }
+  //   return <div>{digits}</div>
+  // }
 
   return (
     <div className="App">
-
-      <div className='container'>
+      <Buttons value="" />
+      {/* <div className='container'>
         <div className='display'>
           {value === "" ? 0 : value}
         </div>
 
         <div className='functions'>
           <button onClick={(event: React.MouseEvent<HTMLElement>) => clear()}>AC</button>
-          {/* <button onClick={(event: React.MouseEvent<HTMLElement>) => remove()}>C</button> */}
           <button onClick={(event: React.MouseEvent<HTMLElement>) => updateValue('*(')}>{`(`}</button>
           <button onClick={(event: React.MouseEvent<HTMLElement>) => updateValue(')')}>{`)`}</button>
           <button onClick={(event: React.MouseEvent<HTMLElement>) => updateValue('**')}>xⁿ</button>
@@ -77,7 +78,7 @@ function App() {
             <button onClick={(event: React.MouseEvent<HTMLElement>) => calculate()}>=</button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- client/src/components/Buttons/Buttons.tsx
+++ client/src/components/Buttons/Buttons.tsx
@@ -1,21 +1,22 @@
 import React, { ReactNode } from 'react';
+import { ClassExpression } from 'typescript';
 
 interface ButtonFunctionsProps {
   value: string,
   clear(): void,
-  updateValue(): string,
+  updateValue(digit: string): string,
   calculate(): void,
 }
 
 export default function Buttons(props: ButtonFunctionsProps) {
 
-  // function createDigits(): ReactNode {
-  //   let digits: JSX.Element[] = [];
-  //   for (let i = 9; i > 0; i--) {
-  //     digits.push(<button key={i} onClick={(event: React.MouseEvent<HTMLElement>) => updateValue(`${i}`)}>{i}</button>)
-  //   }
-  //   return <div>{digits}</div>
-  // }
+  function createDigits(): ReactNode {
+    let digits: JSX.Element[] = [];
+    for (let i = 9; i > 0; i--) {
+      digits.push(<button key={i} onClick={(event: React.MouseEvent<HTMLElement>) => props.updateValue(`${i}`)}>{i}</button>)
+    }
+    return <div>{digits}</div>
+  }
 
   return (
     <div className='container'>
